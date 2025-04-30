a62943db72bbd561a43cd706979173ada9b15b25
TechiVivek <148483692+techivivek@users.noreply.github.com>
2025-04-30 16:17:07 +0530

    Update gemini_cmd.bat

--- src/gemini_cmd.bat
+++ src/gemini_cmd.bat
@@ -1,5 +1,5 @@
 @echo off
 :loop
 set /p query=Search: 
-if not "%query%"=="" python "D:\Programming\Python\gemini.py" %query%
+if not "%query%"=="" python "Paste Here Gemini.py path" %query%
 goto loop
